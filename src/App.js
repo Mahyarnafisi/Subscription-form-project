e92c308acf209f87d44a3d71241761eba8d19624
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-07-19 16:57:26 +0200

    add new Components feature with a image background

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import container from "./Style/Container.module.css";
 import button from "./Style/Button.module.css";
 
+import Feature from "./Components/Feature";
 import SubscriptionContent from "./Components/SubscriptionContent";
 import SimpleInput from "./Components/SimpleInput";
 import "./App.css";
@@ -9,6 +10,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      <Feature />
       <div className={`container ${container.blackbg} `}>
         <SubscriptionContent />
         <SimpleInput />
---
+++ src/Components/Feature.js
@@ -0,0 +1,8 @@
+import React from "react";
+import featureStyle from "./Feature.module.css";
+
+function Feature(props) {
+  return <div className={featureStyle.wrapper}></div>;
+}
+
+export default Feature;
