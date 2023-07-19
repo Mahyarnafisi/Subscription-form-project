b9395b123e98dcfff5c6edd6c990062e1446f8e6
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-07-19 16:30:51 +0200

    new setting Style in button in input and structure

--- src/Components/SimpleInput.js
+++ src/Components/SimpleInput.js
@@ -5,12 +5,13 @@ import inputStyle from "../Style/Input.module.css";
 function SimpleInput(props) {
   return (
     <div className={SimpleInputStyle.wrapper}>
+      <h1>You will get Latest News and article regarding our produce.</h1>
       <form className={SimpleInputStyle.form}>
         <div className={SimpleInputStyle.control}>
           <label htmlFor="name" className={inputStyle.label}>
-            Enter your name
+            Enter your name:
           </label>
-          <input type="text" id="name" className={inputStyle.input} placeholder="Your name please" />
+          <input type="text" id="name" className={inputStyle.input} placeholder="Enter your name please" />
         </div>
         <div className={SimpleInputStyle.action}>
           <button className={ButtonStyle["btn-primary"]}>
@@ -18,6 +19,8 @@ function SimpleInput(props) {
           </button>
         </div>
       </form>
+      <br />
+      <h2 className={SimpleInputStyle.notice}>You will be able to unsubscribe our monthly news any time.</h2>
     </div>
   );
 }
--- src/Components/SubscriptionContent.js
+++ src/Components/SubscriptionContent.js
@@ -5,6 +5,7 @@ function SubscriptionContent(props) {
     <div className={SubscriptionStyle.wrapper}>
       <h1 className={SubscriptionStyle.title}>Subscription</h1>
       <p className={SubscriptionStyle.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod eaque, provident cupiditate est voluptates earum expedita vero placeat voluptatibus nam?</p>
+      <br />
       <div className={SubscriptionStyle.icons}>
         <i class="bi bi-instagram"></i> <i class="bi bi-google"></i> <i class="bi bi-linkedin"></i> <i class="bi bi-facebook"></i>
       </div>
