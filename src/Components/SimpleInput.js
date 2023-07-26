66ce4d0b801bcef544c745f82865c3d5ec151b49
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-07-26 14:46:49 +0200

    ..

--- src/Components/SimpleInput.js
+++ src/Components/SimpleInput.js
@@ -3,53 +3,74 @@ import SimpleInputStyle from "./SimpleInputStyle.module.css";
 import ButtonStyle from "../Style/Button.module.css";
 import inputStyle from "../Style/Input.module.css";
 function SimpleInput(props) {
-  //State Management
-  const [nameInput, setNameInput] = useState("");
-  const [emailNameValid, setEmailNameValid] = useState(false);
-  const [emailInputTouched, setEmailInputTouched] = useState(false);
+  //State Management//
   const [formIsValid, setFormIsValid] = useState(false);
   const [data, setData] = useState([]);
+  //Name Input States
+  const [nameInput, setNameInput] = useState("");
+  const [nameValid, setNameValid] = useState(false);
+  const [nameInputTouched, setNameInputTouched] = useState(false);
+  // Email Input States
+  const [emailInput, setEmailInput] = useState("");
+  const [emailvalid, setEmailValid] = useState(false);
+  const [emailInputTouched, setEmailInputTouched] = useState(false);
+
   // Dynamic Style
-  const inputIsValid = !emailNameValid && emailInputTouched;
+  const inputIsValid = !nameValid && nameInputTouched;
   const inputValidationStyle = !inputIsValid ? inputStyle.input : inputStyle.inputInvalid;
   const labelValidationStyle = !inputIsValid ? inputStyle.label : inputStyle.labelInvalid;
   //
   useEffect(() => {
-    !emailNameValid ? setFormIsValid(false) : setFormIsValid(true);
-  }, [emailNameValid]);
+    !nameValid ? setFormIsValid(false) : setFormIsValid(true);
+  }, [nameValid]);
 
-  //
-  const inputBlurHandler = (e) => {
+  //Email Input func//
+  const onEmailInputChange = (e) => {
+    e.preventDefault();
+    setEmailInput(e.target.value);
+    setEmailValid(true);
+  };
+  const emailInputBlurHandler = () => {
     setEmailInputTouched(true);
   };
-
   //
-  const inputFocusHandler = (e) => {
-    nameInput.trim().length > 1 && setEmailInputTouched(false);
-  };
-  //Input change function
-  const onInputChange = (e) => {
+
+  //Input change func//
+  const onNameInputChange = (e) => {
     setNameInput(e.target.value);
-    setEmailNameValid(true);
+    setNameValid(true);
   };
+  const nameInputBlurHandler = (e) => {
+    setNameInputTouched(true);
+  };
+  const inputFocusHandler = (e) => {
+    nameInput.trim().length > 1 && setNameInputTouched(false);
+  };
+  //
 
   //Submission form function
   const onSubmitHandler = (e) => {
     e.preventDefault();
+    setNameInputTouched(true);
     setEmailInputTouched(true);
-
     //Validation condition
     if (nameInput.trim().length === 0) {
-      setEmailNameValid(false);
+      setNameValid(false);
       return;
     }
-    setEmailNameValid(true);
+    if (emailInput.trim().length === 0) {
+      setEmailValid(false);
+      return;
+    }
+    setNameValid(true);
     const emailSubscription = {
-      email: nameInput,
+      name: nameInput,
+      email: emailInput,
       id: Math.random() * 100,
     };
     data.push(emailSubscription);
     setNameInput("");
+    setEmailInput("");
   };
 
   return (
@@ -59,11 +80,18 @@ function SimpleInput(props) {
       <form className={SimpleInputStyle.form} onSubmit={onSubmitHandler}>
         <div className={SimpleInputStyle.control}>
           <label htmlFor="name" className={labelValidationStyle}>
+            Yourname:
+          </label>
+          <input type="text" id="name" className={inputValidationStyle} placeholder="Enter your name please" onChange={onNameInputChange} value={nameInput} onBlur={nameInputBlurHandler} onFocus={inputFocusHandler} />
+        </div>
+
+        <div className={SimpleInputStyle.control}>
+          <label htmlFor="email" className={labelValidationStyle}>
             Email address:
           </label>
-
-          <input type="text" id="name" className={inputValidationStyle} placeholder="Enter your name please" onChange={onInputChange} value={nameInput} onBlur={inputBlurHandler} onFocus={inputFocusHandler} />
+          <input type="email" id="email" className={inputValidationStyle} placeholder="Enter your email please" onChange={onEmailInputChange} value={emailInput} onBlur={emailInputBlurHandler} onFocus={inputFocusHandler} />
         </div>
+
         <div className={SimpleInputStyle.action}>
           <button disabled={!formIsValid} className={ButtonStyle["btn-primary"]}>
             Subscription Email <i class="bi bi-chevron-right"></i>
